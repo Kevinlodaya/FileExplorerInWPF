ef5fcb4caf3fa1d5d355ef2770456aa83dcb8747
Kevin Lodaya <41232783+Kevinlodaya@users.noreply.github.com>
2021-08-17 21:27:14 +0530

    Add files via upload

---
+++ HeaderToImageConverter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace WpfTreeView
+{
+    
+    [ValueConversion(typeof(string), typeof(BitmapImage))]
+    public class HeaderToImageConverter : IValueConverter
+    {
+        public static HeaderToImageConverter Instance = new HeaderToImageConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Get the full path of the file 
+            var path = (string)value;
+
+            // If no path found the nothing 
+            if (path == null)
+                return null;
+
+            //time to get teh name of file 
+            var name = MainWindow.GetFileFolderName(path);
+
+            var image = "Images/file.png";
+
+            if (string.IsNullOrEmpty(name))
+                image = "Images/drive.png";
+            else if (new FileInfo(path).Attributes.HasFlag(FileAttributes.Directory))
+                image = "Images/folder-closed.png";
+
+            return new BitmapImage(new Uri($"pack://application:,,,/{image}"));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
